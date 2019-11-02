b3ae34d6720195d70f713b0f543a4f54712dbcb6
Jammie sandy <33603488+jamessandy@users.noreply.github.com>
2019-11-02 06:20:06 +0100

    Add files via upload

---
+++ refcator.py
@@ -0,0 +1,30 @@
+num1 = 4
+num2 = 4
+result = num1 + num2
+print(result)
+
+
+
+num1 = int(input('enter the first number:'))
+num2 = int(input('enter the second number:'))
+result = num1 + num2
+print('your answer is:', result)
+
+
+num1 = int(input('Enter number 1:'))
+num2 = int(input('Enter number 2:'))
+result = num1 + num2
+print(result)
+
+
+
+def add (x, y):
+    return x + y
+
+    
+    
+num1 = int(input('Enter number 1:'))
+num2 = int(input('Enter number 2:'))
+result = num1 add num2
+print(result)
+
